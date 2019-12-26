60b7bf780171973fcc9258b3c200147b9c19cb9f
Piyush Singh <piyushsingh8796@gmail.com>
2019-12-26 11:05:32 +0530

    created maven proj

---
+++ src/main/java/com/groundhog/woodchucksbeta/WoodchucksBetaApplication.java
@@ -0,0 +1,13 @@
+package com.groundhog.woodchucksbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodchucksBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodchucksBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groundhog/woodchucksbeta/WoodchucksBetaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.groundhog.woodchucksbeta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WoodchucksBetaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
